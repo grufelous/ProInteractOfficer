7cc5c0e49f21c8b1ad6ead98f50ba9f35b84dc89
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-02-25 23:29:55 +0530

    Add geofence transition check in service

--- app/src/main/java/com/orion/prointeractofficer/GeofenceTransitionsIntentService.java
+++ app/src/main/java/com/orion/prointeractofficer/GeofenceTransitionsIntentService.java
@@ -2,6 +2,13 @@ package com.orion.prointeractofficer;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofenceStatusCodes;
+import com.google.android.gms.location.GeofencingEvent;
+
+import java.util.List;
 
 
 /**
@@ -15,6 +22,7 @@ public class GeofenceTransitionsIntentService extends IntentService {
     // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
     public static final String ACTION_FOO = "com.orion.prointeractofficer.action.FOO";
     public static final String ACTION_BAZ = "com.orion.prointeractofficer.action.BAZ";
+    private static final String TAG = GeofenceTransitionsIntentService.class.getSimpleName();
 
     // TODO: Rename parameters
     public static final String EXTRA_PARAM1 = "com.orion.prointeractofficer.extra.PARAM1";
@@ -27,17 +35,48 @@ public class GeofenceTransitionsIntentService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         if (intent != null) {
-            final String action = intent.getAction();
-            if (ACTION_FOO.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionFoo(param1, param2);
-            } else if (ACTION_BAZ.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionBaz(param1, param2);
+            GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+            if(geofencingEvent.hasError()) {
+                int errorCode = geofencingEvent.getErrorCode();
+                String errorMessage = GeofenceStatusCodes.getStatusCodeString(errorCode);
+                Log.e(TAG, "onHandleIntent: Error Code: " + errorCode + " Error Message: " + errorMessage);
+                return;
+            }
+
+            int geofenceTransition = geofencingEvent.getGeofenceTransition();
+
+            if(geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ||
+                    geofenceTransition == Geofence.GEOFENCE_TRANSITION_DWELL ||
+                    geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+                List<Geofence> triggeredGeofences = geofencingEvent.getTriggeringGeofences();
+
+                // TODO: get data from triggeredGeofences and pass as result
+                String geofenceTransitionDetails = "Something triggered";
+
+                // TODO: show notification instead
+                Log.d(TAG, "onHandleIntent: " + geofenceTransitionDetails);
+            }  else {
+                Log.e(TAG, "onHandleIntent: failed with " + geofenceTransition);
+            }
+
+            if(geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+                handleGeofenceEntry();
+            } else if(geofenceTransition == Geofence.GEOFENCE_TRANSITION_DWELL) {
+                handleGeofenceDwell();
+            } else if(geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+                handleGeofenceExit();
             }
         }
+    }
+
+    // TODO: handle these methods
+    private void handleGeofenceExit() {
+    }
+
+    private void handleGeofenceDwell() {
+    }
+
+    private void handleGeofenceEntry() {
     }
 
     /**
