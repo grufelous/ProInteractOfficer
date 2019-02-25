c1087630dc4bb0961a1547c06b5e52711e0cbbd6
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-02-25 22:29:44 +0530

    Add method to create new geofences

--- app/src/main/java/com/orion/prointeractofficer/GeofenceActivity.java
+++ app/src/main/java/com/orion/prointeractofficer/GeofenceActivity.java
@@ -3,11 +3,30 @@ package com.orion.prointeractofficer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.LocationServices;
+
 public class GeofenceActivity extends AppCompatActivity {
+    private GeofencingClient geofencingClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geofence);
+    }
+
+    // set-up geofence instance
+    private GeofencingClient getGeofencingClientInstance() {
+        return LocationServices.getGeofencingClient(this);
+    }
+
+    // create geofence objects
+    private Geofence getNewGeofence(String geofenceId, double geofencePointLongitude, double geofencePointLatitude, float geofencePointRadius) {
+        return new Geofence.Builder()
+                .setRequestId(geofenceId)
+                .setCircularRegion(geofencePointLongitude, geofencePointLatitude, geofencePointRadius)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT)
+                .build();
     }
 }
