3424289c7f19ad7647a3801f82b9b0bfefa1bc1c
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-02-25 22:52:19 +0530

    Add method to add new geofences

--- app/src/main/java/com/orion/prointeractofficer/GeofenceActivity.java
+++ app/src/main/java/com/orion/prointeractofficer/GeofenceActivity.java
@@ -1,8 +1,13 @@
 package com.orion.prointeractofficer;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.CpuUsageInfo;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,12 +15,15 @@ import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingClient;
 import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.util.List;
 
 public class GeofenceActivity extends AppCompatActivity {
     private GeofencingClient geofencingClient;
     private PendingIntent geofencePendingIntent;
+    private List<Geofence> geofencesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +46,7 @@ public class GeofenceActivity extends AppCompatActivity {
     }
 
     // setup initial triggers
-    private GeofencingRequest getGeofencingRequest(List<Geofence> geofencesList) {
+    private GeofencingRequest getGeofencingRequest() {
         GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
         builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
         builder.addGeofences(geofencesList);
@@ -47,11 +55,38 @@ public class GeofenceActivity extends AppCompatActivity {
 
     // geofence pending intent
     private PendingIntent getGeofencePendingIntent() {
-        if(geofencePendingIntent != null) {
+        if (geofencePendingIntent != null) {
             return geofencePendingIntent;
         }
         Intent intent = new Intent(this, GeofenceTransitionsIntentService.class);
         geofencePendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         return geofencePendingIntent;
+    }
+
+    // add geofence
+    private void addGeofence() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        geofencingClient.addGeofences(getGeofencingRequest(), getGeofencePendingIntent())
+                .addOnSuccessListener(this, new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // yay! geofence added!
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // :(
+                    }
+                });
     }
 }
