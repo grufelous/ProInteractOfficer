cbe9172f2da786e86599c44e0cf00eb54da8efc9
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-02-25 22:09:01 +0530

    Add Geofence Activity

---
+++ app/src/main/java/com/orion/prointeractofficer/GeofenceActivity.java
@@ -0,0 +1,13 @@
+package com.orion.prointeractofficer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GeofenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geofence);
+    }
+}
